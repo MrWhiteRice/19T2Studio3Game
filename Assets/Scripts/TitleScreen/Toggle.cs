42dce86653817ad32f389a8cfea3e051a2685387
Azaloft <40777990+Azaloft@users.noreply.github.com>
2019-07-12 12:02:19 +1000

    Upgrade Overlay

---
+++ Assets/Scripts/TitleScreen/Toggle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toggle : MonoBehaviour
+{
+    public GameObject userInterfaceObject;
+    public GameObject userInterfaceObject2;
+    public void toggle()
+    {
+        if (userInterfaceObject.activeSelf == true)
+        {
+            userInterfaceObject.SetActive(false);
+            userInterfaceObject2.SetActive(true);
+            return;
+        }
+
+        if (userInterfaceObject.activeSelf == false)
+        {
+            userInterfaceObject.SetActive(true);
+            userInterfaceObject2.SetActive(false);
+        }
+    }
+}
