3e3f62ed1961d67595ab84cd55ab0fdba24039e2
MrWhiteRice <jacob_kotro@hotmail.com>
2019-07-25 00:34:19 +1000

    working on gacha test menu

---
+++ Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+	public GameObject[] menu;
+	int selected;
+
+	void ChangeMenu(int index)
+	{
+		foreach(GameObject obj in menu)
+		{
+			obj.SetActive(false);
+		}
+
+		menu[index].SetActive(true);
+	}
+}
