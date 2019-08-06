dba8401b980a72714c9bc683ce142a5e3220f8cb
MrWhiteRice <jacob_kotro@hotmail.com>
2019-08-06 21:56:49 +1000

    implemented scaling fix

---
+++ Assets/Scripts/CamFixer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFixer : MonoBehaviour
+{
+    void Start()
+    {
+		GetComponent<Camera>().orthographicSize = (26f) * (float)Screen.height / (float)Screen.width * 0.5f;
+	}
+}
