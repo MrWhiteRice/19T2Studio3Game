abf3e30e305c37e3be803dbc778a7a699ca3e526
Azaloft <40777990+Azaloft@users.noreply.github.com>
2019-08-18 14:45:59 +1000

    fix things

---
+++ Assets/Scripts/UI/ColourChanger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColourChanger : MonoBehaviour
+{
+    public void Colour()
+    {
+        GetComponent<Image>().color = new Color(0.2623f,0.8301f,0.7862f);
+    }
+
+    public void ResetColour()
+    {
+        GetComponent<Image>().color = new Color(0.2392f, 1, 0.9411f);
+    }
+}
