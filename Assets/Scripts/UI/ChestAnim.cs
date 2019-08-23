36f3768a00a976851243b5868b2acc91e57f63b9
Azaloft <40777990+Azaloft@users.noreply.github.com>
2019-08-23 11:42:07 +1000

    animation for gacha event

---
+++ Assets/Scripts/UI/ChestAnim.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestAnim : MonoBehaviour
+{
+    public GameObject animCanvas;
+    public GameObject chestObject;
+    public GameObject lootObject;
+    public bool animRun;
+    bool rollOne;
+    bool rollTen;
+    string rollType;
+    LootBox lootScript;
+    Animator animatorChest;
+    //set 1 roll
+    public void SingleRoll(string weight)
+    {
+        ChestAnimation();
+        rollOne = true;
+        rollType = weight;
+    }
+
+    //set 10 roll
+    public void TenRoll(string weight)
+    {
+        ChestAnimation();
+        rollTen = true;
+        rollType = weight;
+    }
+
+    void ChestAnimation()
+    {
+        animCanvas.SetActive(true);
+        animatorChest = chestObject.GetComponent<Animator>();
+        animRun = true;
+    }
+
+    private void Update()
+    {
+        if(animRun == true)
+        {
+            if (Input.GetMouseButton(0) && animatorChest.GetCurrentAnimatorStateInfo(0).IsName("Struggle")|| animatorChest.GetCurrentAnimatorStateInfo(0).IsName("still"))
+            {
+                animatorChest.SetTrigger("Open");
+                animRun = false;
+            }
+        }
+
+        if (animatorChest.GetCurrentAnimatorStateInfo(0).IsName("openstill"))
+        {
+            StartCoroutine(wait());
+            lootScript = lootObject.GetComponent<LootBox>();
+            if(rollOne == true)
+            {
+                lootScript.Roll(rollType);
+                animCanvas.SetActive(false);
+            }
+
+            if (rollTen == true)
+            {
+                lootScript.MultiRoll(rollType);
+                animCanvas.SetActive(false);
+            }
+        }
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(1);
+    }
+}
